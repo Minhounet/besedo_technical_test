70ba5b47ceaa2a323041383fa2081fd837b3c15b
Quang Minh Tran <qmtran@groupealterna.com>
2023-03-21 00:45:04 +0100

    Define column properties in database

--- src/main/java/com/qmt/besedo/model/message/MessageDatabaseObject.java
+++ src/main/java/com/qmt/besedo/model/message/MessageDatabaseObject.java
@@ -1,19 +1,28 @@
 package com.qmt.besedo.model.message;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Entity
 @Data
 public class MessageDatabaseObject {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false, nullable = false)
     private long internalId;
 
+    @Length(min = 1, max = 100)
     private String id;
+
+    @Column(nullable = false)
     private String email;
+
+    @Length(max = 300)
     private String title;
+
+    @Length(max = 10000)
     private String body;
 
     public Message toMessage() {
